2b94e40a727b60d9b97beffd9fe3bc10186e3173
Lauren Rose <larose@radford.edu>
2018-11-17 13:30:20 -0500

    refactor tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,16 +3,20 @@ require_relative '../lib/game.rb'
 
 describe 'My behaviour' do
 
-  it 'should create a board' do
-    @game = Game.new([[0,0],[0,0]])
+  context 'board setup' do
+    before :each do
+      @board_array = [[0,0,0],[0,0,0],[0,0,0]]
+      @game = Game.new(@board_array)
+    end
 
-    expect(@game.board).equal?([[0,0],[0,0]])
+    it 'should create a board' do
+      expect(@game.board).equal?(@board_array)
+    end
+
+    it 'should check adjacent' do
+      temp = @game.check_neighbor(0, 1)
+      expect(temp).to be(0)
+    end
   end
 
-  it 'should check adjacent' do
-    @game = Game.new([[0,0,0],[0,0,0]])
-
-    temp = @game.check_neighbor(0, 1)
-    expect(temp).to be(0)
-  end
 end
