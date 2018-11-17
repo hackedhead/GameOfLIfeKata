a0754cbe7b5e0fbed453689bb9b4634b197d1240
Lauren Rose <larose@radford.edu>
2018-11-17 10:35:59 -0500

    add cell class and test xits

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,9 +1,16 @@
 #require 'rspec'
+require_relative '../../lib/game_cell'
 
-describe 'My behaviour' do
+describe 'GameCell' do
 
-  it 'should do something' do
+  context 'live cells' do
+    xit 'should die if fewer than two neighbors'
+    xit 'should stay alive with two to three neighbors'
+    xit 'should die if more than three neighbors'
+  end
 
-    true.should == false
+  context 'dead cells' do
+    xit 'should be born with exactly three neighbors'
+    xit 'should stay dead with not three neighbors'
   end
 end
