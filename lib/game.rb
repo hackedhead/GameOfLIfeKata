c81ad9329d08d500e97433725edcfc489842d2e7
Lauren Rose <larose@radford.edu>
2018-11-17 13:45:46 -0500

    direction fixes

--- lib/game.rb
+++ lib/game.rb
@@ -6,26 +6,26 @@ class Game
     @board = board
   end
 
-  def check_up_down(x, y)
+  def check_up_down(row, col)
     live_count = 0
 
-    if x != 0
-      live_count += @board[x+1][y]
+    if row != 0
+      live_count += @board[row-1][col]
     end
-    if x < board[0].length
-      live_count += @board[x-1][y]
+    if row < board[0].length
+      live_count += @board[row+1][col]
     end
     live_count
   end
 
-  def check_left_right(x, y)
+  def check_left_right(row, col)
     live_count = 0
 
-    if y != 0
-      live_count += @board[x][y-1]
+    if col != 0
+      live_count += @board[row][col-1]
     end
-    if y < board[0].length
-      live_count += @board[x][y+1]
+    if col < board[0].length
+      live_count += @board[row][col+1]
     end
     live_count
   end
